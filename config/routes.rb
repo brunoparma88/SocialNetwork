ef5f0211d88f2b6b91f3f0762adcf7254ab3da77
bruno <brunoparma@hotmail.com>
2021-06-25 11:19:15 -0300

    chance routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
-# frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
-  mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
-    registrations: 'api/v1/registrations',
-    sessions: 'api/v1/sessions'
-  }
+mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
+  registrations:  'api/v1/registrations',
+  sessions:  'api/v1/sessions',
+  passwords:  'api/v1/passwords'
+}
 
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
-      devise_scope :user do
-        resource :user, only: [] do
-          controller :sessions do
-            collection do
-              post :facebook
-            end
-          end
+namespace :api do
+  namespace :v1, defaults: { format: :json } do
+    devise_scope :user do
+      get :status, to: 'api#status'
+      resources :users, only: :show
+      resource :user, only: :update do
+        get :profile
+        controller :sessions do
+          post :facebook, on: :collection
         end
       end
     end
   end
+end
 end

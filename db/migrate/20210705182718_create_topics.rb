0ed0aa0ebab784e51505fe916f5bd77f334a69e3
bruno <brunoparma@hotmail.com>
2021-07-14 16:28:09 -0300

    minimal changes

--- db/migrate/20210705182718_create_topics.rb
+++ db/migrate/20210705182718_create_topics.rb
@@ -1,7 +1,7 @@
 class CreateTopics < ActiveRecord::Migration[6.1]
   def change
     create_table :topics do |t|
-      t.string :name, null: false, index: true
+      t.string :name, null: false, index: true, unique: true
       t.timestamps
     end
   end

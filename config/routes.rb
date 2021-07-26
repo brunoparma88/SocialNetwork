01aa80cfba0cd4135ecdb86c05332a94542b81ef
bruno <brunoparma@hotmail.com>
2021-07-26 12:03:19 -0300

    remove line on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, only: %i[new create update]
   mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
     registrations: 'api/v1/registrations',
     sessions: 'api/v1/sessions'
